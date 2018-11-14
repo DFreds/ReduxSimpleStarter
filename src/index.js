6b10c11c0a0272502164f4e5be43800f4a972e36
Derek Fredrickson <dfreds08@gmail.com>
2018-11-14 01:17:54 -0500

    More code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-const API_KEY = 'AIzaSyAuajNQbKtBirpK1s4xwa06zt99jFEdL5g';
+import VideoList from './components/video_list';
+import API_KEY from './api';
 
 class App extends Component {
     constructor(props) {
         super(props);
+
         this.state = { videos: [] };
+
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             this.setState({ videos });
         });
@@ -17,6 +20,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
